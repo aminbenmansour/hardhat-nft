140cc55ddaeaabb52c025adf06b609f03a7b9618
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-29 23:29:56 +0100

    safely minting an NFT with openZeppelin

--- nft/contracts/BasicNFT.sol
+++ nft/contracts/BasicNFT.sol
@@ -4,6 +4,16 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNFT is ERC721 {
+    uint256 private tokenCounter;
+
     constructor() ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
     }
+
+    function mintNFT() public returns(uint256) {
+        _safeMint(msg.sender, tokenCounter);
+        tokenCounter = tokenCounter + 1;
+        return tokenCounter;
+    }
+
 }

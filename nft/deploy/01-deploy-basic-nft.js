41c1ef0bef1bc3369b8032531b0871897cf3e354
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-29 23:57:49 +0100

    deploying BasicNFT contract

---
+++ nft/deploy/01-deploy-basic-nft.js
@@ -0,0 +1,17 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("--------------------------")
+    const args = []
+    const basicNFT = await deploy("BasicNft", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1
+    })
+}

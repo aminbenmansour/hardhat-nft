1cb43640be92e6b6951e306be239f5c97f473671
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-30 22:43:41 +0100

    getting chainlink vrf contracts for test and deployment

--- nft/deploy/02-deploy-random-ipfs.js
+++ nft/deploy/02-deploy-random-ipfs.js
@@ -1,23 +1,22 @@
-const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
 
-    log("--------------------------")
-    const args = []
-    const basicNFT = await deploy("BasicNFT", {
-        from: deployer,
-        args,
-        log: true,
-        waitConfirmations: network.config.blockConfirmations || 1
-    })
+    let vrfCoordinatorV2Address, subscriptionId
 
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying ...")
-        await verify(basicNFT.address, args)
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
     }
-    log("--------------------------")
 }
